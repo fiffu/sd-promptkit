083d7ed4e98e11ac7b68e35d1fd6deccfc34a9fe
fiffu <fhejehfif@gmail.com>
2023-04-18 20:49:21 +0800

    Use jest for tests

--- tests/taglint.test.js
+++ tests/taglint.test.js
@@ -1,59 +1,91 @@
 const {FormattedTag, TagLint} = require('../taglint');
-const {should} = require('./libtest');
 
+describe('FormattedTag', () => {
+  for (const tc of [
+    {
+      desc: 'default tag weight is considered 0',
+      input: 'masterpiece',
+      expectTagName: 'masterpiece',
+      expectWeight: 0,
+      expectNormalized: 'masterpiece',
+    },
+    {
+      desc: 'input should be lowercased',
+      input: 'Masterpiece',
+      expectTagName: 'masterpiece',
+      expectWeight: 0,
+      expectNormalized: 'masterpiece',
+    },
+    {
+      desc: 'whitespace should be fixed',
+      input: '(masterpiece : 1.4)',
+      expectTagName: 'masterpiece',
+      expectWeight: 1.4,
+      expectNormalized: '(masterpiece: 1.4)',
+    },
+    {
+      desc: 'excess openers in tag weight should be closed',
+      input: '(((masterpiece)',
+      expectTagName: 'masterpiece',
+      expectWeight: 0,
+      expectNormalized: '(((masterpiece)))',
+    },
+    {
+      desc: 'excess closers in tag weight should be trimmed',
+      input: '(masterpiece)))',
+      expectTagName: 'masterpiece',
+      expectWeight: 0,
+      expectNormalized: '(masterpiece)',
+    },
+    {
+      desc: 'braces in tag name should be untouched',
+      input: 'le malin (azur lane)',
+      expectTagName: 'le malin (azur lane)',
+      expectWeight: 0,
+      expectNormalized: 'le malin (azur lane)',
+    },
+    {
+      desc: 'braces in tag name should be untouched, even with numbered weights',
+      input: '(le malin (azur lane): 1.4)',
+      expectTagName: 'le malin (azur lane)',
+      expectWeight: 1.4,
+      expectNormalized: '(le malin (azur lane): 1.4)',
+    },
+    {
+      desc: 'braces in tag name should be untouched, even with braced weights',
+      input: '(((le malin (azur lane))))',
+      expectTagName: 'le malin (azur lane)',
+      expectWeight: 0,
+      expectNormalized: '(((le malin (azur lane))))',
+    },
+    {
+      desc: 'braces in tag name should be untouched, even for multiple pairs',
+      input: "le malin (the knight's true nature) (azur lane)",
+      expectTagName: "le malin (the knight's true nature) (azur lane)",
+      expectWeight: 0,
+      expectNormalized: "le malin (the knight's true nature) (azur lane)",
+    },
+    {
+      desc: 'braces in tag name should have excess openers closed',
+      input: 'le malin (azur lane',
+      expectTagName: 'le malin (azur lane)',
+      expectWeight: 0,
+      expectNormalized: 'le malin (azur lane)',
+    },
+    {
+      desc: 'braces in tag name should have excess closers trimmed',
+      input: 'le malin azur lane)',
+      expectTagName: 'le malin azur lane',
+      expectWeight: 0,
+      expectNormalized: 'le malin azur lane',
+    },
+  ]) {
+    test(tc.desc, () => {
+      const tag = new FormattedTag(tc.input);
 
-for (const tc of [
-  {
-    input: 'Abc',
-    expectTagName: 'abc',
-    expectWeight: 0,
-    expectNormalized: 'abc',
-  },
-  {
-    input: '(def : 1.4)',
-    expectTagName: 'def',
-    expectWeight: 1.4,
-    expectNormalized: '(def: 1.4)',
-  },
-  {
-    input: '(((too many openers for tag weight)',
-    expectTagName: 'too many openers for tag weight',
-    expectWeight: 0,
-    expectNormalized: '(((too many openers for tag weight)))',
-  },
-  {
-    input: '(too many closers for tag weight)))',
-    expectTagName: 'too many closers for tag weight',
-    expectWeight: 0,
-    expectNormalized: '(too many closers for tag weight)',
-  },
-  {
-    input: '(hjk (test): 1.4)',
-    expectTagName: 'hjk (test)',
-    expectWeight: 1.4,
-    expectNormalized: '(hjk (test): 1.4)',
-  },
-  {
-    input: '(unmatched internal closers) are trimmed: 1.4)',
-    expectTagName: 'unmatched internal closers are trimmed',
-    expectWeight: 1.4,
-    expectNormalized: '(unmatched internal closers are trimmed: 1.4)',
-  },
-  {
-    input: '(unmatched internal (openers are closed: 1.4)',
-    expectTagName: 'unmatched internal (openers are closed)',
-    expectWeight: 1.4,
-    expectNormalized: '(unmatched internal (openers are closed): 1.4)',
-  },
-  {
-    input: 'internal tags (without weight)',
-    expectTagName: 'internal tags (without weight)',
-    expectWeight: 0,
-    expectNormalized: 'internal tags (without weight)',
-  },
-]) {
-  const tag = new FormattedTag(tc.input);
-  should.equal(tag.tagName, tc.expectTagName);
-  should.equal(tag.weight, tc.expectWeight);
-  should.equal(tag.normalized, tc.expectNormalized);
-}
+      expect(tag.tagName).toBe(tc.expectTagName);
+      expect(tag.weight).toBe(tc.expectWeight);
+      expect(tag.normalized).toBe(tc.expectNormalized);
+    })
+  }
+})
